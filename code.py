d05dc8af8bf410f6ef5806bd308ff83a131115c9
Sean Duffie <sean.duffie@tamu.edu>
2023-04-16 12:02:27 -0500

    Fix LED triggering by hcsr04 Reverse white led on ir sensor Added zone constants for distances

--- code.py
+++ code.py
@@ -1,30 +1,26 @@
-# Author: Alec Autry
-# Example code for ESET 462:
+"""
+Authors = Alec Autry, Sean Duffie
+"""
 
 #imports
-import sys
 import board
 import digitalio
-import busio
 import time
 import adafruit_hcsr04
 
-# GPIO Pin = digitalio.DigitalInOut(board.GP#)
-
-
 # IR sensor - Always returns True unless triggered
-ir_OUT = digitalio.DigitalInOut(board.GP15)
-ir_OUT.direction = digitalio.Direction.INPUT
-
+IR = digitalio.DigitalInOut(board.GP15)
+IR.direction = digitalio.Direction.INPUT
+IR.pull = digitalio.Pull.UP
 
 #Distance LEDS
 GreenLED = digitalio.DigitalInOut(board.GP6)
 GreenLED.direction = digitalio.Direction.OUTPUT
 
-Yellow1LED = digitalio.DigitalInOut(board.GP8)
+Yellow1LED = digitalio.DigitalInOut(board.GP7)
 Yellow1LED.direction = digitalio.Direction.OUTPUT
 
-Yellow2LED = digitalio.DigitalInOut(board.GP7)
+Yellow2LED = digitalio.DigitalInOut(board.GP8)
 Yellow2LED.direction = digitalio.Direction.OUTPUT
 
 RedLED = digitalio.DigitalInOut(board.GP9)
@@ -33,52 +29,56 @@ RedLED.direction = digitalio.Direction.OUTPUT
 GarageLights = digitalio.DigitalInOut(board.GP16)
 GarageLights.direction = digitalio.Direction.OUTPUT
 
-# Define Virables
-timepassed = 0.0
-distance = 0.0
-signalon = 0
-signaloff = 0
+# Define Variables
+distance: float = 0
+ZONE1 = 15
+ZONE2 = 25
+ZONE3 = 35
+ZONE4 = 45
 
-#Ultrasonic Setup
-sonar = adafruit_hcsr04.HCSR04(trigger_pin = board.GP3, echo_pin = board.GP2)
+# Ultrasonic HC-RS-4 -  
+sonar = adafruit_hcsr04.HCSR04(trigger_pin = board.GP3, echo_pin = board.GP2, timeout=0.5)
 
 while True:
     try:
         distance = sonar.distance
-        print(distance)
     except RuntimeError:
-        distance = 0
+        pass
+
+    print(distance)
     time.sleep(0.1)
-    if distance == 0:
-        RedLED.value = False
-        Yellow1LED.value = False
-        Yellow2LED.value = False
-        GreenLED.value = False
-    elif distance > 70:
-        RedLED.value = True
-        Yellow1LED.value = False
-        Yellow2LED.value = False
-        GreenLED.value = False
 
-    elif distance > 50 and (distance < 70):
-        RedLED.value = True
-        Yellow1LED.value = True
-        Yellow2LED.value = False
-        GreenLED.value = False
-
-    elif distance >25 and (distance < 50):
-        RedLED.value = True
-        Yellow1LED.value = True
-        Yellow2LED.value = True
-        GreenLED.value = False
-
-    elif distance < 25:
-        RedLED.value = True
-        Yellow1LED.value = True
-        Yellow2LED.value = True
+    if distance > ZONE4:
         GreenLED.value = True
-    if ir_OUT.value == False:
-        GarageLights.value = True
-    elif ir_OUT:
-        GarageLights.value = False
+        Yellow1LED.value = False
+        Yellow2LED.value = False
+        RedLED.value = False
 
+    elif distance > ZONE3:
+        GreenLED.value = True
+        Yellow1LED.value = True
+        Yellow2LED.value = False
+        RedLED.value = False
+
+    elif distance > ZONE2:
+        GreenLED.value = False
+        Yellow1LED.value = True
+        Yellow2LED.value = True
+        RedLED.value = False
+
+    elif distance > ZONE1:
+        GreenLED.value = False
+        Yellow1LED.value = False
+        Yellow2LED.value = True
+        RedLED.value = True
+
+    else:
+        GreenLED.value = False
+        Yellow1LED.value = False
+        Yellow2LED.value = False
+        RedLED.value = True
+
+    if IR.value is False:
+        GarageLights.value = False
+    else:
+        GarageLights.value = True
